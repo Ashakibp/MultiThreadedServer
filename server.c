1b2eb87dea42a8fa4f8364e2e60a7376aac2f1fb
Ashakibp <aaronshakib@gmail.com>
2019-03-07 14:50:11 -0500

    Fixed the majority of bugs, added the queues

--- server.c
+++ server.c
@@ -23,7 +23,8 @@
 //pthread_cond_t qu_empty_cond = PTHREAD_COND_INITIALIZER;
 //pthread_cond_t qu_full_cond = PTHREAD_COND_INITIALIZER;
 //pthread_mutex_t qu_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t pthread_mutex= PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t highPriorityMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lowPriorityMutex = PTHREAD_MUTEX_INITIALIZER;
 //sem_t mutex;
 
 
@@ -47,8 +48,9 @@ struct {
 struct job{
     int socketfd;
     int hit;
-    int priority;
-} ;
+    int isImage;
+    char* buffer;
+};
 
 
 struct queue{
@@ -57,7 +59,24 @@ struct queue{
 	struct job* jobs;
 	int size;
 	int maxSize;
-} queue1;
+} highPQ;
+
+struct queue2{
+    int head;
+    int tail;
+    struct job* jobs;
+    int size;
+    int maxSize;
+} lowPQ;
+
+int policy;
+/**
+ * 1 - FIFO OR ANY
+ * 2 - HPIC
+ * 3 - HPHC
+ * Highest Priority to Image Content(HPIC) Just for reference
+ * Highest Priority to HTML Content (HPHC)
+ */
 
 static int dummy; //keep compiler happy
 
@@ -71,11 +90,13 @@ void logger(int type, char *s1, char *s2, int socket_fd)
 		break;
 	case FORBIDDEN: 
 		dummy = write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this simple static file webserver.\n</body></html>\n",271);
-		(void)sprintf(logbuffer,"FORBIDDEN: %s:%s",s1, s2); 
+		(void)sprintf(logbuffer,"FORBIDDEN: %s:%s",s1, s2);
+		close(socket_fd);
 		break;
-	case NOTFOUND: 
+	case NOTFOUND:
 		dummy = write(socket_fd, "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n",224);
-		(void)sprintf(logbuffer,"NOT FOUND: %s:%s",s1, s2); 
+		(void)sprintf(logbuffer,"NOT FOUND: %s:%s",s1, s2);
+		close(socket_fd);
 		break;
 	case LOG: (void)sprintf(logbuffer," INFO: %s:%s:%d",s1, s2,socket_fd); break;
 	}	
@@ -91,16 +112,132 @@ void logger(int type, char *s1, char *s2, int socket_fd)
 	}
 //	if(type == ERROR || type == NOTFOUND || type == FORBIDDEN);
 }
+struct job* web1(int fd, int hit, int threadID){
+	struct job* jawb = calloc(1,sizeof(struct job));
+	jawb->socketfd = fd;
+	jawb->hit = hit;
+	int j, file_fd, buflen;
+	long i, ret, len;
+	char * fstr;
+	char* buffer = calloc(BUFSIZE+1, 1); /* calloc so zero filled and shared amongst threads*/
+	jawb->buffer = buffer;
+	ret =read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
+	if(ret == 0 || ret == -1) {	/* read failure stop now */
+		logger(FORBIDDEN,"failed to read browser request","",fd);
+		return NULL;
+	}
+	if(ret > 0 && ret < BUFSIZE)	/* return code is valid chars */
+		buffer[ret]=0;		/* terminate the buffer */
+	else buffer[0]=0;
+	for(i=0;i<ret;i++)	/* remove CF and LF characters */
+		if(buffer[i] == '\r' || buffer[i] == '\n')
+			buffer[i]='*';
+	logger(LOG,"request",buffer,hit);
+	if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) ) {
+		logger(FORBIDDEN,"Only simple GET operation supported",buffer,fd);
+		return NULL;
+	}
+	for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */
+		if(buffer[i] == ' ') { /* string is "GET URL " +lots of other stuff */
+			buffer[i] = 0;
+			break;
+		}
+	}
+	for(j=0;j<i-1;j++) 	/* check for illegal parent directory use .. */
+		if(buffer[j] == '.' && buffer[j+1] == '.') {
+			logger(FORBIDDEN,"Parent directory (..) path names not supported",buffer,fd);
+			return NULL;
+		}
+	if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
+		(void)strcpy(buffer,"GET /index.html");
 
-/* this is a child web server process, so we can exit on errors */
+	/* work out the file type and check we support it */
+	buflen=strlen(buffer);
+	fstr = (char *)0;
+	for(i=0;extensions[i].ext != 0;i++) {
+		len = strlen(extensions[i].ext);
+		if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
+			fstr = extensions[i].filetype;
+			char* img = "image/";
+			int lenses = strlen(img);
+			if(!strncmp(extensions[i].filetype,img,lenses)){
+				jawb->isImage = 1;
+			} else{
+				jawb->isImage = 0;
+			}
+			break;
+		}
+	}
+	if(fstr == 0){
+		logger(FORBIDDEN,"file extension type not supported",buffer,fd);
+		return NULL;
+	}
+
+	return jawb;
+}
+void web2(struct job JAWB, int threadID){
+	char* buffer = JAWB.buffer;
+	int file_fd ,hit, fd, i, buflen;
+	long ret, len;
+	char * fstr;
+	hit = JAWB.hit;
+	fd = JAWB.socketfd;
+	buflen=strlen(buffer);
+	fstr = (char *)0;
+	for(i=0;extensions[i].ext != 0;i++) {
+		len = strlen(extensions[i].ext);
+		if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
+			fstr =extensions[i].filetype;
+			break;
+		}
+	}
+	if(fstr == 0){
+		logger(FORBIDDEN,"file extension type not supported",buffer,fd);
+		return;
+	}
+	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) {  /* open the file for reading */
+		logger(NOTFOUND, "failed to open file",&buffer[5],fd);
+		return;
+	}
+	logger(LOG,"SEND",&buffer[5],hit);
+	len = (long)lseek(file_fd, (off_t)0, SEEK_END); /* lseek to the file end to find the length */
+	(void)lseek(file_fd, (off_t)0, SEEK_SET); /* lseek back to the file start ready for reading */
+	(void)sprintf(buffer,"HTTP/1.1 200 OK\nServer: nweb/%d.0\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */
+	logger(LOG,"Header",buffer,hit);
+	char* str = calloc(10,1);
+	sprintf( str, "%d", threadID );
+	logger(LOG,"Handled By Thread: ",str,fd);
+	free(str);
+	dummy = write(fd,buffer,strlen(buffer));
+
+	/* Send the statistical headers described in the paper, example below
+
+    (void)sprintf(buffer,"X-stat-req-arrival-count: %d\r\n", xStatReqArrivalCount);
+    dummy = write(fd,buffer,strlen(buffer));
+    */
+
+	/* send file in 8KB block - last block may be smaller */
+	while ((ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
+		dummy = write(fd,buffer,ret);
+	}
+	free(JAWB.buffer);
+	sleep(1);	/* allow socket to drain before signalling the socket is closed */
+	close(fd);
+}
+
+/**
+ * We need to break this into to functions. a read and a return.
+ * @param fd - Socket
+ * @param hit - Request #
+ * @param threadID - If you dont understand this, find a new major.
+ */
 void web(int fd, int hit, int threadID)
 {
 	int j, file_fd, buflen;
 	long i, ret, len;
 	char * fstr;
 	char* buffer = calloc(BUFSIZE+1, 1); /* calloc so zero filled and shared amongst threads*/
-
-	ret =read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
+	ret = read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
 	if(ret == 0 || ret == -1) {	/* read failure stop now */
 		logger(FORBIDDEN,"failed to read browser request","",fd);
 	}
@@ -123,6 +260,7 @@ void web(int fd, int hit, int threadID)
 	for(j=0;j<i-1;j++) 	/* check for illegal parent directory use .. */
 		if(buffer[j] == '.' && buffer[j+1] == '.') {
 			logger(FORBIDDEN,"Parent directory (..) path names not supported",buffer,fd);
+			return;
 		}
 	if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
 		(void)strcpy(buffer,"GET /index.html");
@@ -137,10 +275,14 @@ void web(int fd, int hit, int threadID)
 			break;
 		}
 	}
-	if(fstr == 0) logger(FORBIDDEN,"file extension type not supported",buffer,fd);
+	if(fstr == 0){
+		logger(FORBIDDEN,"file extension type not supported",buffer,fd);
+		return;
+	}
 
 	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) {  /* open the file for reading */
 		logger(NOTFOUND, "failed to open file",&buffer[5],fd);
+		return;
 	}
 	logger(LOG,"SEND",&buffer[5],hit);
 	len = (long)lseek(file_fd, (off_t)0, SEEK_END); /* lseek to the file end to find the length */
@@ -149,7 +291,7 @@ void web(int fd, int hit, int threadID)
 	logger(LOG,"Header",buffer,hit);
 	char* str = calloc(10,1);
 	sprintf( str, "%d", threadID );
-	logger(LOG,"Handled By Thread: ",str,hit);
+	logger(LOG,"Handled By Thread: ",str,fd);
     free(str);
 	dummy = write(fd,buffer,strlen(buffer));
 	
@@ -169,33 +311,53 @@ void web(int fd, int hit, int threadID)
 
 }
 
-struct job dequeJob(){
+
+//todo: Yudi is going to do remove from the queue here, keep in mind - this is a MAX priority queue
+/**
+ * This is to remove from queue here
+ * @return
+ */
+struct job* dequeJob(){
 	int sock = -1;
 	int hit = -1;
-    pthread_mutex_lock(&pthread_mutex);
-	if(queue1.size){
-		hit = queue1.jobs[queue1.head].hit;
-		sock = queue1.jobs[queue1.head].socketfd;
-		queue1.head = (queue1.head+1) % queue1.maxSize;
-		queue1.size--;
-	}
-    pthread_mutex_unlock(&pthread_mutex);
-	struct job x = {};
-	x.socketfd = sock;
-	x.hit = hit;
-	x.priority = 1;
-	return x;
+	int isImage = -1;
+	struct job *jawb = (struct job*) calloc(1, sizeof(jawb));
+	char* buf = jawb->buffer;
+    pthread_mutex_lock(&highPriorityMutex);
+	if(highPQ.size > 0){
+		hit = highPQ.jobs[highPQ.head].hit;
+		sock = highPQ.jobs[highPQ.head].socketfd;
+		isImage = highPQ.jobs[highPQ.head].isImage;
+        jawb->buffer = highPQ.jobs[highPQ.head].buffer;
+        highPQ.jobs[highPQ.head].buffer = NULL;
+		highPQ.head = (highPQ.head+1) % highPQ.maxSize;
+        highPQ.size-=1;
+	} else if(lowPQ.size > 0 && (!(highPQ.size > 0))){
+			hit = lowPQ.jobs[lowPQ.head].hit;
+			sock = lowPQ.jobs[lowPQ.head].socketfd;
+			isImage = lowPQ.jobs[lowPQ.head].isImage;
+            jawb->buffer = lowPQ.jobs[lowPQ.head].buffer;
+            lowPQ.jobs[lowPQ.head].buffer = NULL;
+			lowPQ.head = (lowPQ.head+1) % lowPQ.maxSize;
+			lowPQ.size--;
+		}
+    pthread_mutex_unlock(&highPriorityMutex);
+    jawb->hit = hit;
+	jawb->socketfd = sock;
+	jawb->isImage = isImage;
+	return jawb;
 }
 /**
  * Threads wait here to collect work for PQ.
  */
 void waitForJobs(int i){
     while(1){
-    	struct job x = dequeJob();
-    	if(x.hit != -1){
-			web(x.socketfd, x.hit, i);
+    	struct job* x = dequeJob();
+    	if(x->hit != -1){
+			web2(*x, i);
+			x->hit = -1;
     	} else{
-    	    //Add Condition Variable here
+    	    //
     	}
     }
 }
@@ -216,34 +378,77 @@ void buildThreadPool(pthread_t *pool, int sizeOfPool){
     }
 }
 
-void enqueJob(struct job *job){
 
-    pthread_mutex_lock(&pthread_mutex);
-	if(queue1.size != queue1.maxSize){
-		queue1.jobs[queue1.tail].hit = job->hit;
-		queue1.jobs[queue1.tail].socketfd = job->socketfd;
-        queue1.jobs[queue1.tail].priority = job->priority;
-		queue1.tail = (queue1.tail+1) % queue1.maxSize;
-		queue1.size++;
-		if(queue1.size == 1){
+
+void highPriorityEnque(struct job *job){
+	pthread_mutex_lock(&highPriorityMutex);
+	if(highPQ.size != highPQ.maxSize){
+		highPQ.jobs[highPQ.tail].hit = job->hit;
+		highPQ.jobs[highPQ.tail].socketfd = job->socketfd;
+        highPQ.jobs[highPQ.tail].buffer = job->buffer;
+		highPQ.tail = (highPQ.tail+1) % highPQ.maxSize;
+		highPQ.size++;
+		if(highPQ.size == 1){
 
 		}
 	}
-    pthread_mutex_unlock(&pthread_mutex);
+	pthread_mutex_unlock(&highPriorityMutex);
+}
+void lowPriorityEnque(struct job *job){
+	pthread_mutex_lock(&highPriorityMutex);
+	if(lowPQ.size != lowPQ.maxSize){
+		lowPQ.jobs[lowPQ.tail].hit = job->hit;
+		lowPQ.jobs[lowPQ.tail].socketfd = job->socketfd;
+        lowPQ.jobs[lowPQ.tail].buffer = job->buffer;
+		lowPQ.tail = (lowPQ.tail+1) % lowPQ.maxSize;
+		lowPQ.size++;
+		if(lowPQ.size == 1){
+
+		}
+	}
+	pthread_mutex_unlock(&highPriorityMutex);
+}
+
+//todo: NO PQ IMPLEMENTATION
+/**
+ * Insert into priority queue here
+ * @param job
+ */
+void enqueJob(struct job *job){
+	if(policy==1){
+		highPriorityEnque(job);
+	}
+	else if(policy ==2){
+		if(job->isImage){
+			highPriorityEnque(job);
+		}
+		else{
+			lowPriorityEnque(job);
+		}
+	}
+	else if(policy == 3){
+		if(!job->isImage){
+			highPriorityEnque(job);
+		}
+		else{
+			lowPriorityEnque(job);
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
 
-    pthread_mutex_init(&pthread_mutex, NULL);
-    pthread_mutex_unlock(&pthread_mutex);
-
-    int i, port, listenfd, socketfd, hit, numOfThreads;
+    pthread_mutex_init(&highPriorityMutex, NULL);
+    pthread_mutex_init(&lowPriorityMutex, NULL);
+    pthread_mutex_lock(&highPriorityMutex);
+    pthread_mutex_lock(&lowPriorityMutex);
+	int i, port, listenfd, socketfd, hit, numOfThreads;
 	socklen_t length;
 	static struct sockaddr_in cli_addr; /* static = initialised to zeros */
 	static struct sockaddr_in serv_addr; /* static = initialised to zeros */
 
-	if( argc < 5  || argc > 5 || !strcmp(argv[1], "-?") ) {
+	if( argc < 6  || argc > 6 || !strcmp(argv[1], "-?") ) {
 		(void)printf("hint: nweb Port-Number Top-Directory\t\tversion %d\n\n"
 	"\tnweb is a small and very safe mini web server\n"
 	"\tnweb only servers out file/web pages with extensions named below\n"
@@ -277,15 +482,42 @@ int main(int argc, char **argv)
     if(!argv[4]){
         (void) printf("ERROR: Cannot have fewer than 1 job in queue you DOINK!!!!\n");
     }
+    /**
+     * SCHEDULING POLICY LOGIC
+     */
+    if(!strcmp(argv[5], "FIFO")){
+		policy = 1;
+    }
+	else if(!strcmp(argv[5], "ANY")){
+		policy = 1;
+	}
+	else if(!strcmp(argv[5], "HPIC")){
+		policy = 2;
+	}
+	else if(!strcmp(argv[5], "HPHC")){
+		policy = 3;
+	}
+	else{
+        (void)printf("\n This is bad \n");
+        exit(0);
+	}
+
 	numOfThreads = atoi(argv[3]);
     int bufferSize = atoi(argv[4]);
     pthread_t pool[numOfThreads];
 	buildThreadPool(pool, numOfThreads);
-    queue1.maxSize = bufferSize;
-    queue1.head = 0;
-    queue1.tail = 0;
-    queue1.size = 0;
-    queue1.jobs = (struct job*) calloc(bufferSize,sizeof(struct job));
+    highPQ.maxSize = bufferSize;
+    highPQ.head = 0;
+    highPQ.tail = 0;
+    highPQ.size = 0;
+    highPQ.jobs = (struct job*) calloc(bufferSize,sizeof(struct job));
+    lowPQ.maxSize = bufferSize;
+    lowPQ.head = 0;
+    lowPQ.tail = 0;
+    lowPQ.size = 0;
+    lowPQ.jobs = (struct job*) calloc(bufferSize,sizeof(struct job));
+    pthread_mutex_unlock(&highPriorityMutex);
+    pthread_mutex_unlock(&lowPriorityMutex);
 	/* Become deamon + unstopable and no zombies children (= no wait()) */
 //	if(fork() != 0)//We add code here
 //		return 0; /* parent returns OK to shell */
@@ -308,14 +540,16 @@ int main(int argc, char **argv)
 		logger(ERROR,"system call","bind",0);
 	if( listen(listenfd,64) <0)
 		logger(ERROR,"system call","listen",0);
+
 	for(hit=1; ;hit++) {
 		length = sizeof(cli_addr);
 		if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0) {
             logger(ERROR, "system call", "accept", 0);
 		}
 		else {
-            struct job job1 = {socketfd, hit, 1};
-            enqueJob(&job1);
+			 struct job* jawb = web1(socketfd,hit,-1);
+             enqueJob(jawb);
+//            wait(10000);//flush
             //Wake any sleeping threads
         }
 	}
